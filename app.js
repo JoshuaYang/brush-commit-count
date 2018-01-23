7c6ca8ddf68e9c66522efe191af8066b18cd4402
Joshua Yang <yss901025@gmail.com>
2018-01-23 09:55:57 +0800

    u

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const CronJob = require('cron').CronJob;
 
 const file = path.join(__dirname, 'demo.txt');
 
+function generateNum(minNum, maxNum) {
+    return parseInt(Math.random() * (maxNum - minNum) + minNum);
+}
 
 function autoCommitAndPush() {
   let count = generateNum(7, 13);
@@ -15,24 +18,22 @@ function autoCommitAndPush() {
 
       fs.writeFileSync(file, content);
 
-      execSync(`git add ${file}`);
-      execSync(`git commit -m "${content}"`);
+      execSync(`git --git-dir=${__dirname}/.git --work-tree=${__dirname} add ${file}`);
+      execSync(`git --git-dir=${__dirname}/.git --work-tree=${__dirname} commit -m "${content}"`);
   }
 
-  execSync('git push origin master');
-}
-
-function generateNum(minNum, maxNum) {
-    return parseInt(Math.random() * (maxNum - minNum) + minNum);
+  execSync(`git --git-dir=${__dirname}/.git --work-tree=${__dirname} push origin master`);
 }
 
 
 
-const job = new CronJob({
-  cronTime: '0 0 0 * * *',
-  onTick: autoCommitAndPush,
-});
+autoCommitAndPush();
 
-job.start();
+// const job = new CronJob({
+//   cronTime: '0 0 0 * * *',
+//   onTick: autoCommitAndPush,
+// });
+//
+// job.start();
 
-console.log('===== start auto commit & push to gitbub =====');
+// console.log('===== start auto commit & push to gitbub =====');
